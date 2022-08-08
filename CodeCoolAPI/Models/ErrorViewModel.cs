0c88921350dabeb5cfab9b2870cfeda3f5bc95e4
Mateusz Arak <mateusz.arak@gmail.com>
2022-08-08 11:59:30 +0200

    Initial commit - delete unnecessery files

--- CodeCoolAPI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CodeCoolAPI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
